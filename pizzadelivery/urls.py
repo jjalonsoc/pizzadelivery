8161c5f7f5f8a54986952a1f700a6c1ab7188f1f
jjalonsoc <jjalonsoc@gmail.com>
2020-07-27 20:34:56 +0200

    user management, sign up, login, logout

--- pizzadelivery/urls.py
+++ pizzadelivery/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import shop.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', shop.views.home, name='home'),
     path('<int:blog_id>/', shop.views.detail, name='detail'),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
